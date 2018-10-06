426057a9b22bb2cfd458c1e9ac48eb18836cba96
harshk8 <harshkhatke8@gmail.com>
2018-10-06 23:40:00 +0530

    new modification | second commit

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+kut)idmwn07ga77sr_$+1p&(8o7=hxn_3om+_9*=yn54&lxhe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -87,13 +87,24 @@ WSGI_APPLICATION = 'chat_app.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat_db',
+        'USER': 'dexter',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 
 # Internationalization
@@ -117,4 +128,6 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
